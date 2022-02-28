ff1d457b41a09d6b3f4e989c2277bdc7cfc22079
Scott Carpenter <carpenter.scott22@gmail.com>
2022-02-28 15:03:06 -0600

    added content to src

--- src/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package Main;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.print("Hello, World!");
-    }
-}
---
+++ src/main/HelloWorld.java
@@ -0,0 +1,13 @@
+package main;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 22;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "My wife and I were both born on day 22 of our respective months";
+        System.out.println(myString);
+    }
+}
