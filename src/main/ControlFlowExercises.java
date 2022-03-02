8fd9c6aaa26e51a32e9217e1598027d566aaec7c
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-02 09:46:53 -0600

    finished while exercises

---
+++ src/main/ControlFlowExercises.java
@@ -0,0 +1,12 @@
+package main;
+
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i <= 15) {
+            System.out.print(i + " ");
+            i++;
+        }
+
+    }
+}
